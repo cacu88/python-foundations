34e6b5aab99c1d346a7cb9aec73f3c41d358e1b7
Facundo Sevrini <52105765+cacu88@users.noreply.github.com>
2026-09-02 12:47:34 -0300

    feat: practice variables lists and dictionaries

--- src/python_foundations/__init__.py
+++ src/python_foundations/__init__.py
@@ -1,2 +1,31 @@
 def main() -> None:
-    print("Hello from python-foundations!")
+    name: str = "Facundo"
+    age: int = 37
+    height: float = 1.70
+    is_learning: bool = True
+    
+    languages: list[str] = [
+        "Python", 
+        "TypeScript", 
+        "JavaScript"
+    ]
+
+    languages.append("React")
+    
+    developer: dict[str, str | list[str]] = {
+        "name": name,
+        "role": "Junior Developer",
+	"languages": languages,
+    }
+
+
+    print(f"Nombre: {name}")
+    print(f"Edad: {age}")
+    print(f"Altura: {height}")
+    print(f"¿Está aprendiendo?: {is_learning}")
+    print(f"Primer lenguaje: {languages[0]}")
+    print(f"Ultimo lenguaje: {languages[-1]}")
+    print(f"Nombre: {developer['name']}")
+    print(f"Rol: {developer['role']}")
+    print(f"Lenguajes: {', '.join(developer['languages'])}")
+    print(f"Cantidad de lenguajes: {len(developer['languages'])}")
