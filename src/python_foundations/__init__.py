ff41d2be23592984873d376b7bddc03e2b029f92
Facundo Sevrini <52105765+cacu88@users.noreply.github.com>
2026-09-02 21:53:03 -0300

    feat: practice conditional logic

--- src/python_foundations/__init__.py
+++ src/python_foundations/__init__.py
@@ -1,8 +1,9 @@
 def main() -> None:
     name: str = "Facundo"
-    age: int = 37
+    age: int = 38
     height: float = 1.70
     is_learning: bool = True
+    has_time: bool = True
     
     languages: list[str] = [
         "Python", 
@@ -18,7 +19,6 @@ def main() -> None:
 	"languages": languages,
     }
 
-
     print(f"Nombre: {name}")
     print(f"Edad: {age}")
     print(f"Altura: {height}")
@@ -29,3 +29,21 @@ def main() -> None:
     print(f"Rol: {developer['role']}")
     print(f"Lenguajes: {', '.join(developer['languages'])}")
     print(f"Cantidad de lenguajes: {len(developer['languages'])}")
+
+    if is_learning and has_time:
+        print("You can continue learning Python.")
+    else:
+        print("You need to organize your time better.")
+
+    if not is_learning or not has_time:
+        print("You should review your learning plan.")
+
+    if age < 18:
+        print("You are underage.")
+    elif 18 <= age < 30:
+        print("You are starting your professional career.")
+    elif 30 <= age < 40:
+        print("You are in a professional consolidation stage.")
+    else:
+        print("You have a lot of professional experience.")
+
